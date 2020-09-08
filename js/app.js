1d85c707dabd29fdb914b4c9d1d4fbd25e6d40a3
Ajayi Peter <you@example.com>
2020-09-08 21:31:32 +0100

    changes to serviceworker

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
-if('serviceWorker' in navigator){
-    window.addEventListener('load',()=>{
-        navigator.serviceWorker
-        .register('./sw_cached_pages.js')
-        .then(reg=>console.log('service worker: registerd'))
-        .catch(error=>console.log(error))
-    });
-  }
 
 
 
